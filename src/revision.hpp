e472077011c76bf5c8bf1cde49c545c0d2b4bad0
Maarten L. Hekkelman <maarten@hekkelman.com>
2020-10-13 15:53:02 +0200

    -aclean up

--- src/revision.hpp
+++
@@ -1,8 +0,0 @@
-const char kRevision[] = R"(
-build-83-gb5ca7e4-dirty
-commit b5ca7e404f90a4d10d31260fd2b6ab74a8bcb13d
-Author: Maarten L. Hekkelman <maarten@hekkelman.com>
-Date:   2020-10-13 14:27:00 +0200
-
-    alles doet het weer
-)";
