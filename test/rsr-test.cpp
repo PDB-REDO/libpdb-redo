3909b9991c0d94a44ac78692f91a5898c94826a8
Maarten L. Hekkelman <maarten@hekkelman.com>
2025-08-19 12:36:04 +0200

    verbose

--- test/rsr-test.cpp
+++ test/rsr-test.cpp
@@ -204,7 +204,7 @@ TEST_CASE("refine_1")
 
 	auto rmsd = std::sqrt(d_sum / atoms3.size());
 	std::cout << "RMSd: " << rmsd << '\n';
-	CHECK_THAT(rmsd, Catch::Matchers::WithinAbs(0.27 / 2, 0.27 / 2));
+	CHECK_THAT(rmsd, Catch::Matchers::WithinAbs(0.27, 0.27 / 2));
 
 	std::cout << std::string(cif::get_terminal_width(), '=') << '\n';
 }
@@ -306,7 +306,7 @@ TEST_CASE("refine_2")
 
 	auto rmsd = std::sqrt(d_sum / atomsRea.size());
 	std::cout << "RMSd: " << rmsd << '\n';
-	CHECK_THAT(rmsd, Catch::Matchers::WithinAbs(0.35 / 2, 0.35 / 2));
+	CHECK_THAT(rmsd, Catch::Matchers::WithinAbs(0.35, 0.35 / 2));
 
 	std::cout << std::string(cif::get_terminal_width(), '-') << '\n';
 }
