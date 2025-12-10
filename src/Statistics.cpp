bb135bda5f0a047828340a9b0606eca23b99ce02
Maarten L. Hekkelman <maarten@hekkelman.com>
2025-12-10 17:17:41 +0100

    Skip hydrogens in statistics calculation for single asym

--- src/Statistics.cpp
+++ src/Statistics.cpp
@@ -568,7 +568,11 @@ std::vector<ResidueStatistics> StatsCollector::collect(const std::string &asymID
 	residue_list residues;
 	std::vector<cif::mm::atom> atoms;
 
-	for (auto atom_id : mStructure.get_datablock()["atom_site"].find<std::string>(cif::key("label_asym_id") == asymID, "id"))
+	const auto &atom_site = mStructure.get_datablock()["atom_site"];
+
+	for (auto atom_id : atom_site.find<std::string>(
+		cif::key("label_asym_id") == asymID and
+		cif::key("type_symbol") != "H", "id"))
 	{
 		auto &atom = atoms.emplace_back(mStructure.get_atom_by_id(atom_id));
 
